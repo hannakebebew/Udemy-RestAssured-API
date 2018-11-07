9ef7ab4eb6761327f16ea852ef1ea92188bc156b
hannakebebew <hannakebebew@10.0.0.202>
2018-11-07 17:16:16 -0500

    BASIC API: 	Fitch a resource by using GET Method.

--- demo/src/test/java/com/udemy/api/restassured/MapRestAPi.java
+++ demo/src/test/java/com/udemy/api/restassured/MapRestAPi.java
@@ -1,23 +1,50 @@
 package com.udemy.api.restassured;
 
-import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class MapRestAPi {
+	/*archtecture of restApi is
+	 * RestAssured.URI ="base url"
+	 * given.().. can be parameter, cookie, or 
+	 * when()... methods..get("resource")
+	 * then()... assert.. status code, contentType, body..
+	 * we can also have extract(); 
 	
-	@Test
+	*/
+	Properties prop = new Properties(); 
+	
+	@BeforeTest
+	public void getUrl() throws IOException {
+		FileInputStream fis = new FileInputStream("/Users/hannakebebew/git/repository2/demo/src/test/java/udemy/files/environment.properties");
+		prop.load(fis); 
+		
+	}
+
+	@Test 
 	
 	public void openWeatherMap() {
 		
-		RestAssured.baseURI ="https://samples.openweathermap.org";
+		
+		RestAssured.baseURI = prop.getProperty("HOST");
 		
 		RestAssured.given().
 					param("id", "524901").
 					param("lang", "zh_cn").
 					param("appid", "b1b15e88fa797225412429c1c50c122a1").
-		   when().get("/data/2.5/forecast/daily").then().assertThat().statusCode(200); 
-		
+		   when().get("/data/2.5/forecast/daily").
+		   then().assertThat().statusCode(200).contentType(ContentType.JSON).
+		   and().body("city.country", equalTo("RU")).header("server", "openresty/1.9.7.1");
 	}
 
 }
